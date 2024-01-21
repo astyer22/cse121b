07a41516b18147fca8f43c9a87026d667b219b22
astyer22 <astyer@byui.edu>
2024-01-20 21:28:29 -0700

    push

---
+++ w02-task/scripts/w02-task.js
@@ -0,0 +1,58 @@
+
+/* W02-Task - Profile Home Page */
+
+/* Step 1 - Setup type tasks - no code required */
+
+/* Step 2 - Variables */
+const fullName = '<strong>Austin Jay Styer</strong>';
+let currentYear = '2024';
+const profilePicture = 'images/profile-image.jpg';
+let favDesert = 'cookies';
+
+
+
+/* Step 3 - Element Variables */
+const nameElement = document.getElementById('name')
+const foodElement = document.getElementById('food')
+const yearElement = document.getElementById('year') 
+const imageElement = document.getElementById('picture') 
+
+
+let favFood = ["pizza, movie theater popcorn, grilled cheese"]
+
+/* Step 4 - Adding Content */
+console.log(imageElement);
+
+// Set the innerHTML properpty for the nameELement
+nameElement.innerHTML = fullName;
+
+// Set the content of foodElement to display my favorite foods
+foodElement.innerHTML =  favFood.join(', ')
+
+
+yearElement.textContent = currentYear;
+
+imageElement.setAttribute('src', profilePicture);
+imageElement.setAttribute('alt', `Profile image of ${fullName}`);
+
+// Adding array to the HTML display
+foodElement.textContent += `\n${favFood.join(', ')}`;
+
+// modifyed to html display
+console.log("My new favorite food list is :\n", favFood.join(','));
+
+/* Step 5 - Array */
+
+
+
+// Adding a new favorite food to the array
+favFood.push(favDesert);
+
+// Removing the first element in the favvorite array
+favFood.shift();
+
+// Remove the last element in the favorite food array.
+favFood.pop();
+
+console.log("My newest favorite foods list:\n", favFood.join(', '));
+
---
+++ w02-task/wk2-pracice.js
@@ -0,0 +1,47 @@
+let x = 4
+let y = 6
+
+// 1 Add two numbers
+let z = x + y
+// 2 Multiply two numbers
+let c = x * y
+// 3 Subtract one from a number
+let e = y - x
+// 4 concatinate two strings together.
+let concatSring = "gold" + "fish"
+console.log(concatSring);
+// 5 assign a value to a variable
+let green = "go"
+// 6 increment the value in a variable by 3
+// example
+let j = 3
+const k = x++;
+
+console.log('x:${X}, y:${y}');
+// Excepeted output "x:3, y=4"
+
+let u = 3
+let v = ++u
+
+console.log('u:${u}, v:${v}');
+// expected output u:4, v:4
+
+x += 3
+console.log(x);
+// 7 compare two values to see if they are the same
+let isEqual = (8 === 8);
+console.log(isEqual);
+// 8 check to see if one number is less than another number
+let lessThan = (6 , 9);
+console.log(lessThan);
+// 9 Check to see if two values are NOT equal
+let notEqual = (9 !== 4)
+console.log(notEqual);
+// 10 check to see if a value is less than 10 and greater than 0
+let d = 7
+if(x < 10 && d > 0 ) {
+console.log("True");
+} else {
+    console.log("false");
+}
+
